0b431010a4b043f875fe27798bb3ec56cc8b48a3
athar.ateeq <athar.ateeq@mergestack.com>
2024-07-23 18:58:02 +0500

    use moment

--- src/utils/dateFormatter.ts
+++ src/utils/dateFormatter.ts
@@ -1,4 +1,4 @@
-import { format } from 'date-fns';
+import moment from 'moment';
 
 interface FormatDateTime {
   formattedDate: string;
@@ -6,8 +6,8 @@ interface FormatDateTime {
 }
 
 export const formatDateTime_MonthDayYear_HourMinute_AMPM = (dateString: string): FormatDateTime => {
-  const date = new Date(dateString);
-  const formattedDate = format(date, 'MMM dd, yyyy');
-  const formattedTime = format(date, 'hh:mm a');
+  const date = moment(dateString);
+  const formattedDate = date.format('MMM DD, YYYY');
+  const formattedTime = date.format('hh:mm A');
   return { formattedDate, formattedTime };
 };
